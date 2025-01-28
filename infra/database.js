a42914b149345139dd173a4f9b6c16fd142bd368
Mateus ieno <125301838+MateusIR@users.noreply.github.com>
2025-01-28 14:28:59 +0000

    add banco de dados producão e ajustes no arquivo `datbase.js`

--- infra/database.js
+++ infra/database.js
@@ -7,13 +7,16 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  await client.connect();
+ 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
